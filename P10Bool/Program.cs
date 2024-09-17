6c76811f3b0b2f346dace042d065b27346c4aa0e
Antonia <antonia.tomus@gmail.com>
2024-09-17 13:48:40 +0200

    add bool

---
+++ P10Bool/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("What's your age?");
+int age = int.Parse(Console.ReadLine());
+
+bool isChild = age <= 12;
+bool isTeenager = age >= 13 && age <= 19;
+bool isAdult = age > 19;
+
+Console.WriteLine("You are a child: " + isChild);
+Console.WriteLine("You are a teenager: " + isTeenager);
+Console.WriteLine("You are an adult: " + isAdult);
