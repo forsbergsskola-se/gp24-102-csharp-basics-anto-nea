800aab661dd2f82e644dc59cf4ca7fd15030a094
Antonia <antonia.tomus@gmail.com>
2024-09-17 13:27:20 +0200

    add
    conversion

---
+++ P8Conversion/GP24-102-CSharp-Basics/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Give me a number");
+string input = Console.ReadLine(); 
+float inputFloat = float.Parse(input);
+Console.WriteLine(inputFloat);
+
+int intInput = Convert.ToInt32(inputFloat);
+intInput =  (int) inputFloat;
+Console.WriteLine(intInput);
