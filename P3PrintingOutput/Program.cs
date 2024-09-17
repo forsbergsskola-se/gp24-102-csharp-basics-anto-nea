8e572e2396f54e13cd64dffa6cc6d24988bb5c11
Antonia <antonia.tomus@gmail.com>
2024-09-17 15:42:31 +0200

    add printOutput

--- P3PrintingOutput/Program.cs
+++ P3PrintingOutput/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello World!");
+// Output: Hello World!
+
+Console.WriteLine(5);
+// Output: 5
+
+Console.Write("Hello");
+Console.Write("World");
+Console.Write(5);
+// Output: HelloWorld5
