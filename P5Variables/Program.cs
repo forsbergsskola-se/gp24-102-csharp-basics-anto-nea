59e1970e5d6017c27d1133343dd57567097de9a9
Antonia <antonia.tomus@gmail.com>
2024-09-18 10:44:47 +0200

    add variables
    redid the P5

--- P5Variables/Program.cs
+++ P5Variables/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string a = "Hello";
+Console.WriteLine(a);
+a = "World";
+
+Console.WriteLine(a);
+
+string b = a;
+Console.WriteLine(b);
+
+a = "Forsbergs";
+Console.WriteLine(a);
