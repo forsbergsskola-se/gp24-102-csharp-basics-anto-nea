fd7e2c1108562fc9bd77f7adff82ea8dd803d81d
anto-nea <antonia.tomus@gmail.com>
2024-09-24 15:03:20 +0200

    redoing exercise

---
+++ P6DataTypes/Program.cs
@@ -0,0 +1,8 @@
+﻿string outputString = "Hello World"; //assigning "Hello World" to a string variable
+Console.WriteLine(outputString); //output string to console
+
+float decimalValue = 0.3f; //assigning 0.3 to a float variable
+Console.WriteLine(decimalValue); //output float to console
+
+bool isAwesome = true; //assigning true to a bool variable
+Console.WriteLine(isAwesome); //output bool to console
