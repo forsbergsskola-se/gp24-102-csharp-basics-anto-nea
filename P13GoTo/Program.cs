66d6017e88e9fe4deed029713c4026e0ce4f53ac
anto-nea <antonia.tomus@gmail.com>
2024-09-24 12:40:43 +0200

    go to

---
+++ P13GoTo/Program.cs
@@ -0,0 +1,20 @@
+﻿// P13GoTo 
+
+forceUserToBuy:
+Console.WriteLine("Do you want to buy my game? (y/n)");
+
+string answer = Console.ReadLine().ToLower(); // .To.Lower transforms any upper case in a lower case variant
+
+if (answer == "y") {
+    Console.WriteLine("Excellent choice! You just bought a great game.");
+    goto userBoughtTheGame;
+}
+
+if (answer == "n")
+{
+    goto forceUserToBuy;
+}
+
+userBoughtTheGame:
+
+Console.WriteLine("10000 credits withdrawn from your account.");
