045221e4f89c5624d2b6f8b80da834dae7024ba4
anto-nea <antonia.tomus@gmail.com>
2024-09-25 19:13:30 +0200

    modifying code

--- P10Bool/Program.cs
+++ P10Bool/Program.cs
@@ -1,53 +1,18 @@
-﻿Console.WriteLine("Give me a seed number");
+﻿Console.WriteLine("What's your age?");
+string input = Console.ReadLine();
+int age;
 
-int userSeed = int.Parse(Console.ReadLine());
-
-Random randWithoutSeed = new Random();
-Random specificRandSeed = new Random(userSeed);
-
-//Console.WriteLine(randWithoutSeed.Next());
-//Console.WriteLine(randWithoutSeed.NextDouble());
-//Console.WriteLine(randWithoutSeed.NextSingle());
-
-Console.WriteLine(randWithoutSeed.Next(0,5)); //0..4 first number is inclusive, second is exclusive (excluded).
-
-Random rand = new Random ();
-
-double chanceOfSuccess = 0.5f;
-double roll = rand.NextDouble();
-
-if (chanceOfSucces > roll) {
-    Console.Writeline ("Success");
-    } else {
-    Console.WriteLine ("Failure");
-}
-
-
-// generate random password
-
-Console.WriteLine("Random password generator, generating a new password");
-
-string chars = "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?;:ABCDEFGHIJKLMNOPQRSTUVWXYZ^&;";
-
-Random random = new Random();
-
-string password = string.Empty
-
-fot (int i = 0); i < 6; i++) {
-password += chars[random.Next(chars.Length)];
-}
-
-Console.WriteLine ($"Your new password is: {password}");
-
-string gotoPassword = string.Empty;
-
-backtrack:
-
-gotoPassword += chars[random.Next(chars.Length)];
-
-if (gotoPassword.Lenght < 6) ;
+if (int.TryParse(input, out age) && age >= 0) //validating the inpute so the user can't enter something unparseable
 {
-    go to backtrack;
-}
+    bool isChild = age <= 12;
+    bool isTeenager = age >= 13 && age <= 19;
+    bool isAdult = age > 19;
 
-Console.WriteLine($"Your other new password is: {gotoPassword}");
+    Console.WriteLine("You are a child: " + isChild);
+    Console.WriteLine("You are a teenager: " + isTeenager);
+    Console.WriteLine("You are an adult: " + isAdult);
+}
+else
+{
+    Console.WriteLine("Please enter a valid non-negative age."); //making sure the user can't add a negative number
+}
--- P13GoTo/Program.cs
+++ P13GoTo/Program.cs
@@ -1,20 +1,88 @@
 ﻿// P13GoTo 
 
-forceUserToBuy:
-Console.WriteLine("Do you want to buy my game? (y/n)");
+// using System.Linq.Expressions;
+//
+// forceUserToBuy:
+// Console.WriteLine("Do you want to buy my game? (y/n)");
+//
+// string answer = Console.ReadLine().ToLower(); // .To.Lower transforms any upper case in a lower case variant
+//
+// if (answer == "y") {
+//     Console.WriteLine("Excellent choice! You just bought a great game.");
+//     goto userBoughtTheGame;
+// }
+//
+// if (answer == "n")
+// {
+//     goto forceUserToBuy;
+// }
+//
+// userBoughtTheGame:
+//
+// Console.WriteLine("10000 credits withdrawn from your account.");
 
-string answer = Console.ReadLine().ToLower(); // .To.Lower transforms any upper case in a lower case variant
-
-if (answer == "y") {
-    Console.WriteLine("Excellent choice! You just bought a great game.");
-    goto userBoughtTheGame;
+for (int 1 = 1; i <= 10; i++) {
+    Console.WriteLine($"Your number is {i}");
 }
 
-if (answer == "n")
+bool shouldBreakout = false;
+int counter = 0;
+while (shouldBreakout) {
+    Console.WriteLine("Continue...");
+    counter++;
+    shouldBreakout = counter == 10;
+}
+
+Console.WriteLine("Finished");
+
+//Switch states
+
+// P13_Pattern
+
+string patternOne = "-#-#-#-#";
+string patternTwo = "#-#-#-#-";
+
+Console.WriteLine("Please enter a size!");
+
+int outputSize = int.Parse(Console.ReadLine());
+
+backtrack:
+
+if(outputSize % 2== 0)
+    Console.WriteLine(patternOne);
+else 
+    Console.WriteLine(patternTwo);
+    
+outputSize --;
+if(outputSize >0)
+goto backtrack;
+
+//P13_1
+
+Console.WriteLine("Welcome to the bank of Cyberica!");
+Console.WriteLine("Please input how much you want to withrawl:");
+
+int withrawAmount;
+    while (!int.TryParse(Console.ReadLine(), out withrawAmount)) {
+        Console.WriteLine("Only numerical values can be used to withdraw money, please try again!").;
+}    
+    = int.TryParse(Console.ReadLine()); 
+    
+    
+// P13_2Triangle
+
+Console.WriteLine("Please input the desired size of your triangle:");
+
+retry:
+bool parsed = int.TryParse(Console.ReadLine(), out int desiredSize);
+
+if (!parsed)
 {
-    goto forceUserToBuy;
-}
+    Console.WriteLine("Please only try with numerical values.");
+    goto retry;
+}   
 
-userBoughtTheGame:
-
-Console.WriteLine("10000 credits withdrawn from your account.");
+continuattion:
+Console.WriteLine(new string('#', desiredSize--));
+if(desiredSize >0)
+    goto continuattion;
